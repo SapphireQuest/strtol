78c3041bc27cea3de94fb0ca4d7c661ec7e1a009
Kacper Turbiarz <turbiarz80@gmail.com>
2026-05-16 15:06:29 +0200

    Feat: Add checking sign and validating base functions

--- strtol.c
+++ strtol.c
@@ -1,9 +1,64 @@
-/* dummy implementation of strtol */
+#include <ctype.h>
+#include <errno.h>
+#include <stddef.h>
 
-long strtol(const char* nPtr, char** endPtr, int base)
+int checkSign(const unsigned char* current_char_ptr, int* sign);
+int validateBase(int base);
+
+long int strtol(const char* nPtr, char** endPtr, int base)
 {
-	if (endPtr) {
-		*endPtr = (char*)nPtr;
+	const unsigned char *current_char_ptr = (const unsigned char*)nPtr;
+	
+	while (isspace(*current_char_ptr))
+	{
+		current_char_ptr++;
+	}
+
+	int sign = 1;
+	if (checkSign(current_char_ptr, &sign))
+	{
+		current_char_ptr++;
+	}
+	
+	if (!validateBase(base))
+	{
+		errno = EINVAL;
+		if (endPtr != NULL)
+		{
+			*endPtr = (char*)current_char_ptr;
+		}
+		return 0;
+	}
+
+	
+
+
+	return 0;
+}
+
+int checkSign(const unsigned char* current_char_ptr, int* sign)
+{
+	if (*current_char_ptr == '-')
+	{
+		*sign = -1;
+		return 1;
+	}
+	else if (*current_char_ptr == '+')
+	{
+		return 1;
 	}
 	return 0;
+}
+
+
+int validateBase(int base)
+{
+	if (base == 0 || (base >= 2 && base <= 36))
+	{
+		return 1;
+	}
+	else
+	{	
+		return 0;
+	}
 }
